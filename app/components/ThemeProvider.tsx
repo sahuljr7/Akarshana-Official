f4d02e57d96d165dbb6153b978b3f5bc68df98cb
sahuljr7 <sahulkumarparida@gmail.com>
2024-12-18 22:59:07 +0530

    four

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+
+// Define the type explicitly using React.ComponentProps
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-
